d1e5b77b53ff19e46a2e575b31ec3a1e8351e3b3
yymin1022 <yymin1022@gmail.com>
2022-08-31 17:42:21 +0900

    Update Functions : Add Post related API Functions - Post, Comment, Recommend

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,42 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Hello, World!");
 });
 
+app.post("/post/getPost", (req: Request, res: Response) => {
+    res.send("Func : Get Post")
+});
+
+app.post("/post/updatePost", (req: Request, res: Response) => {
+    res.send("Func : Update Post")
+});
+
+app.post("/post/deletePost", (req: Request, res: Response) => {
+    res.send("Func : Delete Post")
+});
+
+app.post("/post/getComment", (req: Request, res: Response) => {
+    res.send("Func : Get Comment")
+});
+
+app.post("/post/updateComment", (req: Request, res: Response) => {
+    res.send("Func : Update Comment")
+});
+
+app.post("/post/deleteComment", (req: Request, res: Response) => {
+    res.send("Func : Delete Comment")
+});
+
+app.post("/post/getRecommend", (req: Request, res: Response) => {
+    res.send("Func : Get Recommend")
+});
+
+app.post("/post/updateRecommend", (req: Request, res: Response) => {
+    res.send("Func : Update Recommend")
+});
+
+app.post("/post/deleteRecommend", (req: Request, res: Response) => {
+    res.send("Func : Delete Recommend")
+});
+
 app.post("/profile/getPersonalInfo", (req: Request, res: Response) => {
     res.send("Func : Get Personal Info")
 });
