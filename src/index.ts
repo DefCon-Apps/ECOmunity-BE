831d802fb0f48b512ef7d541e51afcc306a94e19
yymin1022 <yymin1022@gmail.com>
2022-08-31 17:36:22 +0900

    Update Functions : Add Profile related API Functions

--- src/index.ts
+++ src/index.ts
@@ -8,19 +8,19 @@ app.get("/", (req: Request, res: Response) => {
 
 app.post("/profile/getPersonalInfo", (req: Request, res: Response) => {
     res.send("Func : Get Personal Info")
-})
+});
 
 app.post("/profile/getPersonalImage", (req: Request, res: Response) => {
     res.send("Func : Get Personal Image")
-})
+});
 
 app.post("/profile/updatePersonalInfo", (req: Request, res: Response) => {
     res.send("Func : Update Personal Info")
-})
+});
 
 app.post("/profile/updatePersonalImage", (req: Request, res: Response) => {
     res.send("Func : Update Personal Image")
-})
+});
 
 app.listen(8080, () => {
     console.log("Server Listening on Port 8080");
