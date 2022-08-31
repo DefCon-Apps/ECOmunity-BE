9fed631813252cfb5c0af560effd43719d91d27b
yymin1022 <yymin1022@gmail.com>
2022-09-01 02:37:40 +0900

    Update Main Express Code : Apply Router

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express, { Request, Response } from "express";
 
+import boardRouter from "./BoardAPI";
+import profileRouter from "./ProfileAPI";
+
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello, World!");
 });
 
+app.use("/board", boardRouter);
+app.use("/profile", profileRouter);
 
 app.listen(8080, () => {
     console.log("Server Listening on Port 8080");
