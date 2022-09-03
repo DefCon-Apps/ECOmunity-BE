1e26b074189a258be745d721ebcb171d4f9385b5
yymin1022 <yymin1022@gmail.com>
2022-09-03 17:34:40 +0900

    Update FirebaseUtil : Add Base Code for Init FB

---
+++ Util/FirebaseUtil.ts
@@ -0,0 +1,15 @@
+import {FirebaseApp, initializeApp} from "firebase/app";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+const firebaseConfig = {
+    apiKey: process.env.FB_API_KEY,
+    authDomain: process.env.FB_AUTH_DOMAIN,
+    projectId: process.env.FB_PROJECT_ID,
+    appId: process.env.FB_APP_ID
+};
+
+export const initFirebase = () => {
+    console.log(firebaseConfig);
+};
