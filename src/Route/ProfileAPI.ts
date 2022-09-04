ff104951f2d4f17d22b51d16ad0b28ed81e0b042
yymin1022 <yymin1022@gmail.com>
2022-09-05 02:29:21 +0900

    Update ProfileAPI : Fixed Mistypo on Back-End Routing

--- src/Route/ProfileAPI.ts
+++ src/Route/ProfileAPI.ts
@@ -4,22 +4,22 @@ import * as AuthUtil from "../Util/AuthUtil";
 
 const profileRouter = express.Router();
 
-profileRouter.post("getPersonalInfo", (req: Request, res: Response) => {
+profileRouter.post("/getPersonalInfo", (req: Request, res: Response) => {
     const USER_TOKEN = req.body.USE_TOKEN;
     AuthUtil.verifyToken(USER_TOKEN);
 
     res.send("Func : Get Personal Info")
 });
 
-profileRouter.post("getPersonalImage", (req: Request, res: Response) => {
+profileRouter.post("/getPersonalImage", (req: Request, res: Response) => {
     res.send("Func : Get Personal Image")
 });
 
-profileRouter.post("updatePersonalInfo", (req: Request, res: Response) => {
+profileRouter.post("/updatePersonalInfo", (req: Request, res: Response) => {
     res.send("Func : Update Personal Info")
 });
 
-profileRouter.post("updatePersonalImage", (req: Request, res: Response) => {
+profileRouter.post("/updatePersonalImage", (req: Request, res: Response) => {
     res.send("Func : Update Personal Image")
 });
 
