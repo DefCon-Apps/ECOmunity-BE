e2847a6a4df180f7a01cee2b35893061350411c1
yymin1022 <yymin1022@gmail.com>
2022-09-08 00:41:11 +0900

    Update ProfileAPI : Added getUserInfo Dummy Data

--- src/Route/ProfileAPI.ts
+++ src/Route/ProfileAPI.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 
+import { API_DATA } from "../Util/ApiUtil";
 import * as AuthUtil from "../Util/AuthUtil";
 
 const profileRouter = express.Router();
@@ -8,7 +9,17 @@ profileRouter.post("/getUserInfo", (req: Request, res: Response) => {
     const USER_TOKEN = req.body.USER_TOKEN;
     AuthUtil.verifyToken(USER_TOKEN);
 
-    res.send("Func : Get User Info")
+    const API_RESULT_DATA: API_DATA = {
+        RESULT_CODE: 0,
+        RESULT_MSG: "Ready",
+        RESULT_DATA: {
+            USER_NAME: "USER_NAME",
+            USER_EMAIL: "USER_EMAIL",
+            USER_PHONE: "USER_PHONE"
+        }
+    }
+
+    res.send(API_RESULT_DATA)
 });
 
 profileRouter.post("/getUserImage", (req: Request, res: Response) => {
