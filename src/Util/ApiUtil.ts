7dbe5d27c663aed304663095d695a7c4e30b5984
yymin1022 <yymin1022@gmail.com>
2022-09-13 02:04:47 +0900

    Update API Util : Added Data Interface for Post Data

--- src/Util/ApiUtil.ts
+++ src/Util/ApiUtil.ts
@@ -4,6 +4,14 @@ export interface API_DATA {
     RESULT_DATA: API_USER_INFO | object
 }
 
+export interface API_POST_DATA {
+    POST_AUTHOR: string,
+    POST_DATE: string,
+    POST_CONTENT: string,
+    POST_IMAGE: string,
+    POST_TITLE: string
+}
+
 export interface API_USER_INFO {
     USER_NAME: string,
     USER_EMAIL: string,
