5b0bc455d761129a131e7c539e0b7f40f26157c0
yymin1022 <yymin1022@gmail.com>
2022-09-13 02:09:21 +0900

    Update Board API : Setted Data Type for Post Data

--- src/Route/BoardAPI.ts
+++ src/Route/BoardAPI.ts
@@ -39,7 +39,11 @@ boardRouter.post("/updatePost", async (req: Request, res: Response) => {
     const USER_TOKEN = req.body.USER_TOKEN;
 
     const POST_ID = req.body.POST_ID;
-    const POST_DATA = req.body.POST_DATA;
+    const POST_AUTHOR = req.body.POST_DATA.POST_AUTHOR;
+    const POST_DATE = req.body.POST_DATA.POST_DATE;
+    const POST_CONTENT = req.body.POST_DATA.POST_CONTENT;
+    const POST_IMAGE = req.body.POST_DATA.POST_IMAGE;
+    const POST_TITLE = req.body.POST_DATA.POST_TITLE;
 
     const API_RESULT_DATA: API_DATA = {
         RESULT_CODE: 200,
