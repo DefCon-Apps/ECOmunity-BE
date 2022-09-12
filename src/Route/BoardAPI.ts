f09374eea1567cca92ddfbc46439cd610c78f4c4
yymin1022 <yymin1022@gmail.com>
2022-09-13 01:55:38 +0900

    Update Board API : Added Async Attributes

--- src/Route/BoardAPI.ts
+++ src/Route/BoardAPI.ts
@@ -2,27 +2,27 @@ import express, { Request, Response } from "express";
 
 const boardRouter = express.Router();
 
-boardRouter.post("/getPost", (req: Request, res: Response) => {
+boardRouter.post("/getPost", async (req: Request, res: Response) => {
     res.send("Func : Get Post")
 });
 
-boardRouter.post("/updatePost", (req: Request, res: Response) => {
+boardRouter.post("/updatePost", async (req: Request, res: Response) => {
     res.send("Func : Update Post")
 });
 
-boardRouter.post("/deletePost", (req: Request, res: Response) => {
+boardRouter.post("/deletePost", async (req: Request, res: Response) => {
     res.send("Func : Delete Post")
 });
 
-boardRouter.post("/getRecommend", (req: Request, res: Response) => {
+boardRouter.post("/getRecommend", async (req: Request, res: Response) => {
     res.send("Func : Get Recommend")
 });
 
-boardRouter.post("/updateRecommend", (req: Request, res: Response) => {
+boardRouter.post("/updateRecommend", async (req: Request, res: Response) => {
     res.send("Func : Update Recommend")
 });
 
-boardRouter.post("/deleteRecommend", (req: Request, res: Response) => {
+boardRouter.post("/deleteRecommend", async (req: Request, res: Response) => {
     res.send("Func : Delete Recommend")
 });
 
